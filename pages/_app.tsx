5145bb2f1b72af9fa9fdf4ed8ae0e007877a4289
igorizviekov <igorizviekov@gmail.com>
2022-11-11 12:24:35 +0200

    feat: added header & footer boilerplate, added the,=meprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import { Header, Footer } from '../components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  );
 }
