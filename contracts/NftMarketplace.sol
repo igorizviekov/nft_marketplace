82ba032b3e2c904c13f2f398ff91c530ef8dcab1
igorizviekov <igorizviekov@gmail.com>
2022-11-30 13:25:49 +0200

    feat: added smart contract config

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+// Using ERC721 standard
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrive = 0.01 ether;
+    
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        bool sold;
+        uint256 price;
+    }
+
+    event MerketItemCreated (
+        uint256 indexed tokenId,
+        address  seller,
+        address  owner,
+        bool sold,
+        uint256 price
+    );
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+}
