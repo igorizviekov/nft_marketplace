e129998c7558e44ec63d4dd1426fb3f4894b8669
igorizviekov <igorizviekov@gmail.com>
2022-12-16 14:53:26 +0200

    refactor: lifterd state up on the nft list page

--- components/nft-list/index.tsx
+++ components/nft-list/index.tsx
@@ -7,96 +7,33 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Spinner } from '../spinner';
 
-export const NftList = () => {
-  const [nftList, setNftList] = useState<INftCardProps[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState<boolean | string>(true);
+interface INFTListProps {
+  nfts: INftCardProps[];
+  isLoading: boolean;
+}
 
-  const fetchNFTs = async () => {
-    const provider = new ethers.providers.JsonRpcProvider();
-    const contract = fetchContract(provider);
-    /**
-     * List of all available NFTs on marketplace.
-     * Filtered by "not sold"
-     */
-    const data = await contract.getActiveCocktails();
-
-    /**
-     * Map data to the format, which will used on frontend
-     */
-    const items = await Promise.all(
-      data.map(async ({ tokenId, seller, owner, price }: INftCardProps) => {
-        const formattedPrice = ethers.utils.formatUnits(
-          price.toString(),
-          'ether'
-        );
-        const tokenURI: string = await contract.tokenURI(tokenId);
-
-        // get NFT metadata and image
-        const {
-          data: { image, name, description },
-        } = await axios.get(tokenURI);
-
-        return {
-          price: formattedPrice,
-          tokenId: Number(tokenId),
-          img: image,
-          seller,
-          owner,
-          name,
-          description,
-        };
-      })
-    );
-    return items;
-  };
-
-  useEffect(() => {
-    fetchNFTs()
-      .then((items) => {
-        if (items?.length) {
-          setNftList(items);
-        }
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        console.log('failed to fetch NFT', e);
-        setIsError('Failed to fetch NFT');
-        setIsLoading(false);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(isError);
-    }
-  }, [isError]);
-
-  const content = isLoading ? (
-    <Spinner styles="min-h-screen w-full mt-20 animate-fadeIn" />
-  ) : !isLoading && !nftList.length ? (
-    <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold my-5 px-5 sm:text-center">
-      No NFTs Listed for Sale
-    </h1>
-  ) : (
-    <>
-      <div className="w-full flex justify-between flex-center  animate-fadeIn sm:flex-col">
+export const NftList = ({ isLoading, nfts }: INFTListProps) => (
+  <div className="mb-12">
+    <div className="w-full flex flex-wrap justify-start md:justify-center ">
+      {isLoading ? (
+        <Spinner styles="min-h-screen w-full mt-20 animate-fadeIn" />
+      ) : !isLoading && !nfts.length ? (
         <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold my-5 px-5 sm:text-center">
-          Trending NFTs
+          No NFTs Listed for Sale
         </h1>
-        <Search />
-      </div>
-      {nftList.map((nft, i) => (
-        <NftCard key={nft.owner + i} {...nft} />
-      ))}
-    </>
-  );
-
-  return (
-    <div className="mb-12">
-      <div className="w-full flex flex-wrap justify-start md:justify-center ">
-        {content}
-      </div>
+      ) : (
+        <>
+          <div className="w-full flex justify-between flex-center  animate-fadeIn sm:flex-col">
+            <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold my-5 px-5 sm:text-center">
+              Trending NFTs
+            </h1>
+            <Search />
+          </div>
+          {nfts.map((nft, i) => (
+            <NftCard key={nft.owner + i} {...nft} />
+          ))}
+        </>
+      )}
     </div>
-  );
-};
+  </div>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,84 @@
 import { NftList } from '../components/nft-list';
 import { TopSellers } from '../components/top-sellers';
 import { Banner } from '../components/ui/banner/banner';
+import { INftCardProps, NftCard } from '../components/ui/nft-card';
+import { useEffect, useState } from 'react';
+import { Search } from '../components/search';
+import { ethers } from 'ethers';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { fetchContract } from '../utils';
 
 export default function Home() {
+  const [nftList, setNftList] = useState<INftCardProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState<boolean | string>(true);
+
+  const fetchNFTs = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const contract = fetchContract(provider);
+    /**
+     * List of all available NFTs on marketplace.
+     * Filtered by "not sold"
+     */
+    const data = await contract.getActiveCocktails();
+
+    /**
+     * Map data to the format, which will used on frontend
+     */
+    const items = await Promise.all(
+      data.map(async ({ tokenId, seller, owner, price }: INftCardProps) => {
+        const formattedPrice = ethers.utils.formatUnits(
+          price.toString(),
+          'ether'
+        );
+        const tokenURI: string = await contract.tokenURI(tokenId);
+
+        // get NFT metadata and image
+        const {
+          data: { image, name, description },
+        } = await axios.get(tokenURI);
+
+        return {
+          price: formattedPrice,
+          tokenId: Number(tokenId),
+          img: image,
+          seller,
+          owner,
+          name,
+          description,
+        };
+      })
+    );
+    return items;
+  };
+
+  useEffect(() => {
+    fetchNFTs()
+      .then((items) => {
+        if (items?.length) {
+          setNftList(items);
+        }
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        console.log('failed to fetch NFT', e);
+        setIsError('Failed to fetch NFT');
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(isError);
+    }
+  }, [isError]);
+
   return (
     <div className="pt-32 sm:pt-26  w-9/12  sm:w-full m-auto">
       <Banner />
       <TopSellers />
-      <NftList />
+      <NftList nfts={nftList} isLoading={isLoading} />
     </div>
   );
 }
