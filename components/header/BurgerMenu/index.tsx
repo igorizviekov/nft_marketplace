e1e36f8e66a7c256e59e2958f46fc634a250167e
igorizviekov <igorizviekov@gmail.com>
2022-12-23 14:44:43 +0200

    feat: burger menu background

--- components/header/BurgerMenu/index.tsx
+++ components/header/BurgerMenu/index.tsx
@@ -15,36 +15,46 @@ export const BurgerMenu = ({
   actions,
   menuItems,
 }: IBurgerMenuProps) => (
-  <div className="hidden md:flex ml-2">
-    <div
-      className={`${styles['burger-menu']} ${
-        isOpen ? `text-nft-red-violet ${styles['burger-menu__open']}` : ''
-      }`}
-      onClick={onToggle}
-    >
-      <span
-        className={`${styles['burger-menu__line']} ${styles['burger-menu__line1']}`}
-      />
-      <span
-        className={`${styles['burger-menu__line']} ${styles['burger-menu__line2']}`}
-      />
-      <span
-        className={`${styles['burger-menu__line']} ${styles['burger-menu__line3']}`}
-      />
-    </div>
+  <>
     {isOpen && (
-      <div className={styles['burger-menu__items']}>
-        <div className="flex flex-col p-5  overflow-hidden px-5 py-10 gap-10">
-          <ButtonGroup options={actions} />
-          {menuItems}
-        </div>
-        <AiOutlinePause
-          onClick={onToggle}
-          size={55}
-          color="#FFDD00"
-          className="absolute right-2 bottom-20 cursor-pointer"
+      <div
+        className="w-screen h-screen  backdrop-blur-lg absolute left-0 top-0"
+        onClick={onToggle}
+      />
+    )}
+    <div className="hidden md:flex ml-2">
+      <div
+        className={`${styles['burger-menu']} ${
+          isOpen ? `text-nft-red-violet ${styles['burger-menu__open']}` : ''
+        }`}
+        onClick={onToggle}
+      >
+        <span
+          className={`${styles['burger-menu__line']} ${styles['burger-menu__line1']}`}
+        />
+        <span
+          className={`${styles['burger-menu__line']} ${styles['burger-menu__line2']}`}
+        />
+        <span
+          className={`${styles['burger-menu__line']} ${styles['burger-menu__line3']}`}
         />
       </div>
-    )}
-  </div>
+      {isOpen && (
+        <>
+          <div className={styles['burger-menu__items']}>
+            <div className="flex flex-col p-5  overflow-hidden px-5 py-10 gap-10">
+              <ButtonGroup options={actions} />
+              {menuItems}
+            </div>
+            <AiOutlinePause
+              onClick={onToggle}
+              size={55}
+              color="#FFDD00"
+              className="absolute right-2 bottom-20 cursor-pointer"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  </>
 );
