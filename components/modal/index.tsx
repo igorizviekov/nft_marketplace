b24f25e1561c8494d2a44e8d34ae539bbf46d7d8
igorizviekov <igorizviekov@gmail.com>
2022-12-23 12:43:16 +0200

    style: checkout modal mob

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -20,7 +20,7 @@ const modal = ({ header, footer, body, onClose }: IModalProps) => {
         className="w-2/5 md:w-11/12 minlg:2/4 dark:bg-nft-dark bg-white flex flex-col rounded-lg"
         ref={modalRef}
       >
-        <div className="flex justify-end mt-4 mr-4 minlg:mt-6 minlg:mr-6">
+        <div className="flex justify-end mt-4 mr-4 minlg:mt-6 minlg:mr-6 sm:mt-0">
           <div
             className="relative w-3 h-3 minlg:w-6 minlg:h-6 cursor-pointer"
             onClick={onClose}
@@ -29,15 +29,15 @@ const modal = ({ header, footer, body, onClose }: IModalProps) => {
           </div>
         </div>
 
-        <div className="flexCenter w-full text-center p-4">
+        <div className="flexCenter w-full text-center p-4 sm:p-0">
           <h2 className="font-poppins dark:text-white text-nft-black-1 font-normal text-2xl">
             {header}
           </h2>
         </div>
-        <div className="p-10 sm:px-4 border-t border-b dark:border-nft-black-3 border-nft-gray-1">
+        <div className="p-10 sm:py-0 border-t border-b dark:border-nft-black-3 border-nft-gray-1">
           {body}
         </div>
-        <div className="flexCenter p-4">{footer}</div>
+        <div className="flexCenter p-4 sm:p-0">{footer}</div>
       </div>
     </div>
   );
--- components/payment-body/index.tsx
+++ components/payment-body/index.tsx
@@ -9,7 +9,7 @@ const PaymentBody = ({ nft, currency }: IPaymentBodyProps) => {
       <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">
         Item
       </p>
-      <div className="flexBetweenStart my-5">
+      <div className="flexBetweenStart my-5 sm:my-2">
         <div className="flex-1 flexStartCenter">
           <div className="relative w-28 h-28">
             <Image alt="NFt" src={nft.img} height={100} width={100} />
@@ -26,30 +26,30 @@ const PaymentBody = ({ nft, currency }: IPaymentBodyProps) => {
           </div>
         </div>
       </div>
-      <p className="font-poppins dark:text-white text-nft-black-1 ">
+      <p className="font-poppins dark:text-white text-nft-black-1 sm:font-light sm:mt-0">
         Buying this NFT, you will donate 15% of it's price to the following
         charity funds to support Ukraine:
       </p>
-      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold ">
+      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold sm:font-light sm:mt-0">
         <Link href="https://helpingtoleave.org/" target="_blank">
           - Helping to leave ({charityCut} {currency})
         </Link>
       </p>
-      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold ">
+      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold sm:font-light sm:mt-0">
         <Link href="https://savelife.in.ua/en/donate-en/" target="_blank">
           - Come Back Alive ({charityCut} {currency})
         </Link>
       </p>
-      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold ">
+      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 ml-10 font-semibold sm:font-light sm:mt-0">
         <Link href="https://prytulafoundation.org/en" target="_blank">
           - Charity foundation of Serhiy Prytula ({charityCut} {currency})
         </Link>
       </p>
-      <p className="font-poppins dark:text-white text-nft-black-1 mt-2">
+      <p className="font-poppins dark:text-white text-nft-black-1 mt-2 sm:font-light sm:mt-0">
         The rest ({(nft.price * 0.85).toFixed(2)} {currency}) will go to the
         NFT's creator
       </p>
-      <div className="flexBetween mt-10">
+      <div className="flexBetween mt-10 sm:mt-2">
         <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">
           Total
         </p>
