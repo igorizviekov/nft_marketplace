e1560c469b583d3ea6c003508869c06443693581
Manuel <manu.benitez1@hotmail.com>
2023-04-10 14:17:20 +0200

    refactor: dropdown item

--- components/header/DropdownMenu/DropdownMenuItem/DropdownMenuItem.tsx
+++ components/header/DropdownMenu/DropdownMenuItem/DropdownMenuItem.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Icon from '../../../ui/Icon/Icon';
 import { IDropdownMenuItem } from './DropdownMenuItem.types';
-import styles from './DropdownMenuItem.module.scss'
+import styles from './DropdownMenuItem.module.scss';
+import Link from 'next/link';
 
 const DropdownMenuItem = ({ icon, label }: IDropdownMenuItem) => {
   return (
-    <div className={styles.dropdownItem}>
+    <Link href={''} className={styles.dropdownItem}>
       <Icon icon={icon} />
       <p>{label}</p>
-    </div>
+    </Link>
   );
 };
 
