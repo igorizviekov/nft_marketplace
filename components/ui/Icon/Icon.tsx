83cdf0d2acda6045514a99532484bb2702b5afde
Manuel <manu.benitez1@hotmail.com>
2023-04-10 14:28:30 +0200

    refactor: icon

--- components/ui/Icon/Icon.tsx
+++ components/ui/Icon/Icon.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { IconContext } from 'react-icons';
 import { IIconProps } from './Icon.types';
-import styles from './Icon.module.scss';
-const Icon = ({ icon, context, ...props }: IIconProps) => {
+import classNames from 'classnames';
+const Icon = ({ icon, context, className, ...props }: IIconProps) => {
   const providerProps = context
     ? { value: context }
     : { value: { style: { width: '1rem' } } };
+
   return (
     <IconContext.Provider {...providerProps}>
-      <div className={styles.icon} {...props}>
+      <div className={classNames(className)} {...props}>
         {icon}
       </div>
     </IconContext.Provider>
