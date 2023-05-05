9d6dbc2256f8e4087b3052c5c8656586e9a939a1
Manuel <manu.benitez1@hotmail.com>
2023-05-05 17:44:12 +0200

    fix types

--- components/SortBy/SortBy.types.d.ts
+++ components/SortBy/SortBy.types.d.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export interface ISortByProps {
   activeSelect: ActiveSelectOption;
   setActiveSelect: Dispatch<SetStateAction<ActiveSelectOption>>;
