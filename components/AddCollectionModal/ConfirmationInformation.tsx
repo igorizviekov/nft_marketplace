2fcbc923404372500098e0709699c650d76b8e8f
Manuel <manu.benitez1@hotmail.com>
2023-05-13 19:58:47 +0200

    refactor: confirmation modal

--- components/AddCollectionModal/ConfirmationInformation.tsx
+++ components/AddCollectionModal/ConfirmationInformation.tsx
@@ -3,36 +3,40 @@ import { IModalSteps } from './AddCollectionModal.types';
 import { useStoreState } from '../../store';
 import { Button } from '../ui/Button';
 import { useCreateCollection } from '../../service/useCreateCollection';
-
+import styles from './AddCollectionModal.module.scss';
 const ConfirmationInformation = ({ handleSteps }: IModalSteps) => {
   const { generalInformation, royalties, networkInformation } = useStoreState(
     (state) => state.createCollection
   );
   return (
-    <div>
+    <>
       <h2>Please check the information before submitting!</h2>
       <div>
-        <p>General information</p>
-        <p>name: {generalInformation.name}</p>
-        <p>name: {generalInformation.description}</p>
-        <p>name: {generalInformation.website}</p>
+        <h2 className={styles.title}>General information</h2>
+        <p>Name: {generalInformation.name}</p>
+        <p>Description: {generalInformation.description}</p>
+        {generalInformation.website && (
+          <p>Website: {generalInformation.website}</p>
+        )}
       </div>
       <div>
-        <p>Network information</p>
-        <p>symbol: {networkInformation.symbol}</p>
-        <p>network: {networkInformation.network}</p>
+        <h2 className={styles.title}>Network information</h2>
+        <p>Symbol: {networkInformation.symbol}</p>
+        <p>Network: {networkInformation.network}</p>
         <p>Primary Category: {networkInformation.mainCategory}</p>
         <p>Secondary Category: {networkInformation.subCategory}</p>
       </div>
-      <div>
-        <p>Royalties Information</p>
-        {royalties.map((royalty, index) => (
-          <div key={index}>
-            <p>{royalty.walletAddress}</p>
-            <p>{royalty.percentage}</p>
-          </div>
-        ))}
-      </div>
+      {royalties.length !== 0 && (
+        <div>
+          <h2 className={styles.title}>Royalties Information</h2>
+          {royalties.map((royalty, index) => (
+            <div key={index}>
+              <p>Wallet: {royalty.walletAddress}</p>
+              <p>Percentage: {royalty.percentage}</p>
+            </div>
+          ))}
+        </div>
+      )}
 
       <Button
         isPrimary={false}
@@ -45,7 +49,7 @@ const ConfirmationInformation = ({ handleSteps }: IModalSteps) => {
           })
         }
       />
-    </div>
+    </>
   );
 };
 
