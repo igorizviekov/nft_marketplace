f49b35de9c92abb5cc86c8ffb7985fd682f23e07
Manuel <manu.benitez1@hotmail.com>
2023-07-09 20:14:08 +0200

    feat: is token listed

--- service/list/isListed.tsx
+++ service/list/isListed.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { ethers } from 'ethers';
+import React, { useMemo } from 'react';
+import { MarketplaceABI, marketplaceAddress } from '../../mocks/constants.mock';
+import { getErrMessage } from '../useMintNFT';
+import { toast } from 'react-toastify';
 
-const isListed = () => {
-  return (
-    <div>isListed</div>
-  )
-}
+const isListed = async (tokenID: number) => {
+  const marketplaceContract = useMemo(
+    () => new ethers.Contract(marketplaceAddress, MarketplaceABI, provider),
+    []
+  );
+  const provider = useMemo(
+    () =>
+      new ethers.providers.JsonRpcProvider(
+        'https://json-rpc.evm.testnet.shimmer.network'
+      ),
+    []
+  );
+  try {
+    const tx = await marketplaceContract.isTokenListed(tokenID);
+    return tx;
+  } catch (err) {
+    console.log({ err });
+    const message = getErrMessage(err);
+    toast.error(message);
+  }
+};
 
-export default isListed
+export default isListed;
