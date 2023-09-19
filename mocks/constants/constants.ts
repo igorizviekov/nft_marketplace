681af3b8b42043c14d1a9b14823e0624a9cd8fa8
igorizviekov <igorizviekov@gmail.com>
2023-09-19 13:19:47 +0300

    refacor: remove unused event in SC

--- mocks/constants/constants.ts
+++ mocks/constants/constants.ts
@@ -2,9 +2,9 @@ import collections from './ERC721Collections.json';
 import marketplace from './NFTMarketplace.json';
 
 // address of a contract after its been deployed
-export const collectionsAddress = '0x840748F7Fd3EA956E5f4c88001da5CC1ABCBc038';
+export const collectionsAddress = '0xde79380FBd39e08150adAA5C6c9dE3146f53029e';
 
-export const marketplaceAddress = '0x1bEfE2d8417e22Da2E0432560ef9B2aB68Ab75Ad';
+export const marketplaceAddress = '0xbFD3c8A956AFB7a9754C951D03C9aDdA7EC5d638';
 
 export const CollectionsABI = collections.abi;
 export const MarketplaceABI = marketplace.abi;
